20d46a8f102be522ac6bc68a6bd382ff06bc0a06
ishanthedoctor <ishan130890@gmail.com>
2026-03-03 22:55:58 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       setIsScrolled(window.scrollY > 20);
     };
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); 
   }, []);
 
   
