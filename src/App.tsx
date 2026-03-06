6c253e212eb778e7cb2d80baac8ea55316e0628c
ishanthedoctor <ishan130890@gmail.com>
2026-03-06 23:44:10 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import Terms from "./pages/Terms";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Terms from "./pages/Terms";
+import Security from "./pages/Security";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Main Landing Page */}
         <Route path="/" element={<HomePage />} />
+
+        {/* Legal Pages */}
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/security" element={<Security />} />
+
       </Routes>
     </BrowserRouter>
   );
