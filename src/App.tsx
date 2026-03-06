e0c02a4dfa91cced3de98a2ef9ba3223668ad943
ishanthedoctor <ishan130890@gmail.com>
2026-03-06 22:28:32 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import PlatformTour from './components/PlatformTour';
-import PlatformPipeline from './components/PlatformPipeline';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Problem from './components/Problem';
-import Solution from './components/Solution';
-import WorkflowStory from './components/WorkflowStory';
-import ProductDemo from './components/ProductDemo';
-import FeaturesBento from './components/FeaturesBento';
-import DiagnuvoAISection from './components/DiagnuvoAISection';
-import TrustSecurity from './components/TrustSecurity';
-import About from './components/About';
-import FooterCTA from './components/FooterCTA';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 export default function App() {
   return (
-   <div className="min-h-screen bg-[#020617] text-white font-sans">
-      <Navbar />
-      <main>
-        <Hero />
-<Problem />
-<PlatformTour />
-<PlatformPipeline />
-        <Solution />
-        <WorkflowStory />
-        <ProductDemo />
-        <FeaturesBento />
-        <DiagnuvoAISection />
-        <TrustSecurity />
-        <About />
-      </main>
-<FooterCTA />
-</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
