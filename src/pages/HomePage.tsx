fdf8d089f930d2b43c486da3f4e5922122a61020
ishanthedoctor <ishan130890@gmail.com>
2026-03-06 22:22:47 +0000

    Create HomePage.tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Problem from "../components/Problem";
+import PlatformTour from "../components/PlatformTour";
+import PlatformPipeline from "../components/PlatformPipeline";
+import Solution from "../components/Solution";
+import WorkflowStory from "../components/WorkflowStory";
+import ProductDemo from "../components/ProductDemo";
+import FeaturesBento from "../components/FeaturesBento";
+import DiagnuvoAISection from "../components/DiagnuvoAISection";
+import TrustSecurity from "../components/TrustSecurity";
+import About from "../components/About";
+import FooterCTA from "../components/FooterCTA";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-[#020617] text-white font-sans">
+      <Navbar />
+      <main>
+        <Hero />
+        <Problem />
+        <PlatformTour />
+        <PlatformPipeline />
+        <Solution />
+        <WorkflowStory />
+        <ProductDemo />
+        <FeaturesBento />
+        <DiagnuvoAISection />
+        <TrustSecurity />
+        <About />
+      </main>
+      <FooterCTA />
+    </div>
+  );
+}
